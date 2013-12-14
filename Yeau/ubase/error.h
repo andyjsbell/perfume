87b88f9afb01fc4b0b6b3a84eb81c45cb0d4bfd3
Yongze Xu <yongzxu@Yongzes-MacBook-Pro.local>
2013-12-14 20:19:18 +0800

    code refine

--- Yeau/ubase/error.h
+++ Yeau/ubase/error.h
@@ -6,7 +6,11 @@
 
 #define EAU_E_INVALIDPTR        1
 #define EAU_E_INVALIDARG        2
-#define EAU_E_EXISTED           3
-#define EAU_E_FAILED            4
+#define EAU_E_EXIST             3
+#define EAU_E_FAIL              4
+
+#define return_if_fail(p)       {if(!(p)) {return;}}
+#define returnv_if_fail(p, v)   {if(!(p)) {return (v);}}
+#define break_if_fail(p)        {if(!(p)) {break;}}
 
 #endif
