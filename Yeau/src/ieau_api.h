594b93cd16b4299379b885f38974fd5f2ae9ffc2
Yongze Xu <yongze@yongze-VirtualBox.(none)>
2014-01-02 17:10:09 +0800

    update

---
+++ Yeau/src/ieau_api.h
@@ -0,0 +1,36 @@
+#ifndef _IEAU_API_H_
+#define _IEAU_API_H_
+
+class IEauSink {
+public:
+    virtual void OnSignedIn() {};
+    virtual void OnRegistered() {};
+
+    virtual void OnProjectEvent() {};
+    virtual void OnProjectUserEvent() {};
+    virtual void OnProjectBillEvent() {};
+
+protected:
+    virtual ~IEauSink() {}
+};
+
+class IEauApi{
+public:
+    virtual void SetSink(IEauSink *pSink) = 0;
+
+    virtual bool SignOut() = 0;
+    virtual bool SignIn(const char *name, const char *pass) = 0;
+    virtual bool Register(const char *name, const char *pass) = 0;
+    
+    virtual bool GetProjects() = 0;
+    virtual bool GetProjectInfo() = 0;
+    virtual bool GetProjectUsers() = 0;
+    virtual bool GetProjectBills() = 0;
+
+    virtual bool CreateProject() = 0;
+    virtual bool AddProjectBill() = 0;
+    virtual bool AddProjectUser() = 0;
+};
+
+
+#endif // _IEAU_API_H_
